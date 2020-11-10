18fc7dbe11c7a88d909eb2dba26311b3338c62eb
Sabin Neupane <61322712+sabin26@users.noreply.github.com>
2020-11-10 16:49:49 +0545

    Initial Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// create express app
+const app = express();
+
+// Setup server port
+const port = process.env.PORT || 4000;
+
+// enable cors origin access
+app.use(cors({
+    //origin: '[website URL which calls the api]'
+}));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Configuring the database
+const dbConfig = require('./config/db.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database.', err);
+    process.exit();
+});
+
+// define a root/default route
+app.get('/', (req, res) => {
+    res.json({"message": "Hello World"});
+});
+
+// Require Users routes
+const userRoutes = require('./src/routes/user.routes.js')
+// using as middleware
+app.use('/users', userRoutes)
+
+// listen for requests
+app.listen(port, () => {
+    console.log(`Node server is listening on port ${port}`);
+});
